e9db368f772298bc7dd197a4a505c7b53d47d713
Nick Voerman <nick21092001@gmail.com>
2023-05-03 15:12:38 +0200

    Remove false test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,8 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders open formulieren create react app', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Open Formulieren create react app/i);
   expect(linkElement).toBeInTheDocument();
 });
